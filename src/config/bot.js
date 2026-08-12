883427555ca197958052066f04c1e115eb2f68c5
kenji6370-ui <kenji6370@gmail.com>
2026-08-12 15:08:54 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#0e0e0e",
-      secondary: "#0e0e0e",
+      primary: "#292d30",
+      secondary: "#292d30",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
